eeaba28c0d2d6546bea8f0cd1372f64663c7f4b3
Sven Koppens <sven.koppens@gmail.com>
2020-10-20 15:52:35 +0200

    First commit Dbconnection setup part one

---
+++ Cars/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Cars.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cars.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options ) : base(options)
+        {
+
+        }
+
+        public DbSet<CarList> CarList { get; set; }
+
+    }
+}
---
+++ Cars/Models/CarList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cars.Models
+{
+    public class CarList
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+        /*public enum Type
+        {
+            Sedan,
+            Coupe,         
+        }*/
+    }
+}
